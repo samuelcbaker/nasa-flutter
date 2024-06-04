bf57296cddcd25713ab8bc21c20b208ceba9ce68
Samuel Baker <samuelcastro.baker@gmail.com>
2024-06-03 23:28:47 -0300

    feat: nasa image entity

---
+++ lib/domain/entities/nasa_image.dart
@@ -0,0 +1,12 @@
+class NasaImage {
+  final String title;
+  final String explanation;
+  final String url;
+  final String date;
+
+  NasaImage(
+      {required this.title,
+      required this.explanation,
+      required this.url,
+      required this.date});
+}
