dc518be59a8f18ce1474072146e0119fece2871d
Samuel Baker <samuelcastro.baker@gmail.com>
2024-06-04 13:25:23 -0300

    feat: add tests of datasource api

---
+++ test/data/datasources/api_image_datasource_impl_test.dart
@@ -0,0 +1,127 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:nasa_flutter/constants.dart';
+import 'package:nasa_flutter/data/datasources/api_image_datasource_impl.dart';
+import 'package:nasa_flutter/domain/exceptions/exceptions.dart';
+import 'package:nasa_flutter/infra/datasources/api_image_datasource.dart';
+
+class DioMock extends Mock implements Dio {}
+
+void main() {
+  late Dio dio;
+  late ApiImageDatasource datasource;
+
+  setUp(() {
+    dio = DioMock();
+    datasource = ApiImageDatasourceImpl(dio: dio);
+  });
+
+  test('should get images and parse correctly', () async {
+    when(() => dio.get(any(), queryParameters: any(named: 'queryParameters')))
+        .thenAnswer((_) async {
+      final response = [
+        {
+          "date": "2024-06-01",
+          "explanation":
+              "Get out your red/blue glasses and float next to Helene, small, icy moon of Saturn. Appropriately named, Helene is a Trojan moon, so called because it orbits at a Lagrange point. A Lagrange point is a gravitationally stable position near two massive bodies, in this case Saturn and larger moon Dione. In fact, irregularly shaped ( about 36 by 32 by 30 kilometers) Helene orbits at Dione's leading Lagrange point while brotherly ice moon Polydeuces follows at Dione's trailing Lagrange point. The sharp stereo anaglyph was constructed from two Cassini images captured during a close flyby in 2011. It shows part of the Saturn-facing hemisphere of Helene mottled with craters and gully-like features.",
+          "hdurl":
+              "https://apod.nasa.gov/apod/image/2406/N00172886_92_beltramini.jpg",
+          "media_type": "image",
+          "service_version": "v1",
+          "title": "Stereo Helene",
+          "url":
+              "https://apod.nasa.gov/apod/image/2406/N00172886_92_beltramini.jpg"
+        },
+        {
+          "date": "2024-06-02",
+          "explanation":
+              "No one, presently, sees the Moon rotate like this. That's because the Earth's moon is tidally locked to the Earth, showing us only one side.  Given modern digital technology, however, combined with many detailed images returned by the Lunar Reconnaissance Orbiter (LRO), a high resolution virtual Moon rotation movie has been composed. The featured time-lapse video starts with the standard Earth view of the Moon. Quickly, though, Mare Orientale, a large crater with a dark center that is difficult to see from the Earth, rotates into view just below the equator.  From an entire lunar month condensed into 24 seconds, the video clearly shows that the Earth side of the Moon contains an abundance of dark lunar maria, while the lunar far side is dominated by bright lunar highlands. Currently, over 32 new missions to the Moon are under active development from multiple countries and companies, including NASA's Artemis program which aims to land people on the Moon again within the next few years.",
+          "media_type": "video",
+          "service_version": "v1",
+          "title": "Rotating Moon from LRO",
+          "url": "https://www.youtube.com/embed/sNUNB6CMnE8?rel=0"
+        },
+        {
+          "copyright": "(Team F.A.C.T.)",
+          "date": "2024-06-03",
+          "explanation":
+              "Magnificent island universe NGC 2403 stands within the boundaries of the long-necked constellation Camelopardalis. Some 10 million light-years distant and about 50,000 light-years across, the spiral galaxy also seems to have more than its fair share of giant star forming HII regions, marked by the telltale reddish glow of atomic hydrogen gas. The giant HII regions are energized by clusters of hot, massive stars that explode as bright supernovae at the end of their short and furious lives. A member of the M81 group of galaxies, NGC 2403 closely resembles a galaxy in our own local galaxy group with an abundance of star forming regions, M33, the Triangulum Galaxy. Spiky in appearance, bright stars in this portrait of NGC 2403 are in the foreground, within our own Milky Way. Also in the foreground of the deep, wide-field, telescopic image are the Milky Way's dim and dusty interstellar clouds also known as galactic cirrus or integrated flux nebulae. But faint features that seem to extend from NGC 2403 itself are likely tidal stellar streams drawn out by gravitational interactions with neighboring galaxies.",
+          "hdurl":
+              "https://apod.nasa.gov/apod/image/2405/NGC2403-LRGB+Ha+Oiii-v25-f.jpg",
+          "media_type": "image",
+          "service_version": "v1",
+          "title": "NGC 2403 in Camelopardalis",
+          "url":
+              "https://apod.nasa.gov/apod/image/2405/NGC2403-LRGB+Ha+Oiii-v25-f1024.jpg"
+        },
+        {
+          "copyright": "\nRolando Ligustri &\n Lukas Demetz \n",
+          "date": "2024-06-04",
+          "explanation":
+              "Why does Comet Pons-Brooks now have tails pointing in opposite directions?  The most spectacular tail is the blue-glowing ion tail that is visible flowing down the image.  The ion tail is pushed directly out from the Sun by the solar wind. On the upper right is the glowing central coma of Comet 12P/Pons–Brooks. Fanning out from the coma, mostly to the left, is the comet's dust tail. Pushed out and slowed down by the pressure of sunlight, the dust tail tends to trail the comet along its orbit and, from some viewing angles, can appear opposite to the ion tail.  The distant, bright star Alpha Leporis is seen at the bottom of the featured image captured last week from Namibia. Two days ago, the comet passed its closest to the Earth and is now best visible from southern skies as it dims and glides back to the outer Solar System.",
+          "hdurl":
+              "https://apod.nasa.gov/apod/image/2406/Comet12P_Ligustri_1601.jpg",
+          "media_type": "image",
+          "service_version": "v1",
+          "title": "Comet Pons-Brooks Develops Opposing Tails",
+          "url":
+              "https://apod.nasa.gov/apod/image/2406/Comet12P_Ligustri_960.jpg"
+        }
+      ];
+
+      return Response(requestOptions: RequestOptions(), data: response);
+    });
+
+    final response = await datasource.getImages(
+      DateTime(2024, 2, 1),
+      DateTime(2024, 2, 4),
+    );
+
+    expect(response.length, 4);
+    expect(response[0].title, 'Stereo Helene');
+    expect(response[0].explanation,
+        "Get out your red/blue glasses and float next to Helene, small, icy moon of Saturn. Appropriately named, Helene is a Trojan moon, so called because it orbits at a Lagrange point. A Lagrange point is a gravitationally stable position near two massive bodies, in this case Saturn and larger moon Dione. In fact, irregularly shaped ( about 36 by 32 by 30 kilometers) Helene orbits at Dione's leading Lagrange point while brotherly ice moon Polydeuces follows at Dione's trailing Lagrange point. The sharp stereo anaglyph was constructed from two Cassini images captured during a close flyby in 2011. It shows part of the Saturn-facing hemisphere of Helene mottled with craters and gully-like features.");
+    expect(response[0].url,
+        'https://apod.nasa.gov/apod/image/2406/N00172886_92_beltramini.jpg');
+    expect(response[0].date.year, 2024);
+    expect(response[0].date.month, 6);
+    expect(response[0].date.day, 1);
+
+    verify(
+      () => dio.get('/planetary/apod', queryParameters: {
+        'api_key': nasaApiKey,
+        'start_date': '2024-02-01',
+        'end_date': '2024-02-04',
+      }),
+    ).called(1);
+  });
+
+  test('should throw NetworkException if response is null', () {
+    when(() => dio.get(any(), queryParameters: any(named: 'queryParameters')))
+        .thenAnswer((_) async {
+      return Response(requestOptions: RequestOptions(), data: null);
+    });
+
+    expect(
+        () => datasource.getImages(
+              DateTime(2024, 2, 1),
+              DateTime(2024, 2, 4),
+            ),
+        throwsA(isA<NetworkException>()));
+  });
+
+  test('should throw NetworkException if response is not a List', () {
+    when(() => dio.get(any(), queryParameters: any(named: 'queryParameters')))
+        .thenAnswer((_) async {
+      return Response(requestOptions: RequestOptions(), data: {});
+    });
+
+    expect(
+        () => datasource.getImages(
+              DateTime(2024, 2, 1),
+              DateTime(2024, 2, 4),
+            ),
+        throwsA(isA<NetworkException>()));
+  });
+}
