8e5e45614d65ead1c6c3bee2ce1277b00144d063
Samuel Baker <samuelcastro.baker@gmail.com>
2024-06-04 13:24:36 -0300

    feat: add intl and functions to format dates

--- lib/extensions/date_extension.dart
+++ lib/extensions/date_extension.dart
@@ -1,5 +1,17 @@
+import 'package:intl/intl.dart';
+
 extension DateExtension on DateTime {
   DateTime onlyDate() {
     return DateTime(year, month, day);
+  }
+
+  String format() {
+    return DateFormat('yyyy-MM-dd').format(this);
+  }
+}
+
+extension StringDateExtension on String {
+  DateTime? toDate() {
+    return DateFormat('yyyy-MM-dd').tryParse(this);
   }
 }
