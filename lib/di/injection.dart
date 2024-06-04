b1c08e3339f1830987561d9536d89055afc7bc68
Samuel Baker <samuelcastro.baker@gmail.com>
2024-06-03 22:46:29 -0300

    feat: first injection config

---
+++ lib/di/injection.dart
@@ -0,0 +1,7 @@
+import 'package:get_it/get_it.dart';
+
+class AppInjection {
+  static void inject() {
+    final getIt = GetIt.instance;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:nasa_flutter/app.dart';
+import 'package:nasa_flutter/di/injection.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  AppInjection.inject();
+
   runApp(const NasaApp());
 }
